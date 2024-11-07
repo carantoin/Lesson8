d29952203de4d404f955c7294bc7d2bdf1d71dd5
carantoin <90127329+carantoin@users.noreply.github.com>
2024-11-07 17:41:54 +0200

    Add files via upload

---
+++ Lesson8/Lesson8/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    Console.Write("Введите k:");
+    int k = int.Parse(Console.ReadLine());
+    double p = 1;
+    for (int j = 1; j <= k; j++)
+        if (j != 3 && j != 4) p *=(j * j - 4) * j / j - k + 1;
+    double s = 0;
+    for (int i = 1; i <= 9; i++)
+    {
+        if (i != 1) s += i - 3 / i - 7;
+    }
+    double res = p * s;
+    Console.WriteLine($"res={res:F2}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
